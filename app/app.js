1454cb02235f1352ed45ddc107f0cca92759cce1
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2013-10-12 21:02:52 +0200

    listening to the socket events

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 var httpreq = require('httpreq');
 var buttons = require('./buttons');
+var config = require('./config');
 
 // dit moeten we van de server krijgen:
 var username, posturl;
@@ -15,7 +16,7 @@ button2answer['orange'] = 'C';
 // verbinden met de websocket:
 var io = require('socket.io-client');
 console.log("connecting to socket");
-socketclient = io.connect('http://localhost:3000');
+socketclient = io.connect(config.url);
 
 // websocket events:
 socketclient.on('connect', function () {
@@ -23,15 +24,61 @@ socketclient.on('connect', function () {
 	socketclient.send('hi there');
 });
 
-socketclient.on('some-event', function (data) {
-	console.log(data);
-});
-
 socketclient.on('disconnect', function () {
 	console.log('socketclient disconnected');
 });
 
+socketclient.on('point', function(data){
+  //console.log('Point received: ' + JSON.stringify(data));
 
+  switch(data.type){
+  	case "tv:start": events.onTvStart(data);break;	
+    case "quiz:start": events.onQuizStart(data);break;
+    case "quiz:end": events.onQuizEnd(data);break;
+    case "question:soon": events.onQuestionSoon(data);break;
+    case "question:start": events.onQuestionStart(data);break;
+    case "question:end": events.onQuestionEnd(data);break;
+    case "score:update": events.onScoreUpdate(data);break;
+  }
+});
+
+var events = {
+	onTvStart: function(data){
+		console.log("On TV start");
+	},
+
+	onQuizStart: function(data){
+		console.log('Quiz start');
+		//
+	},
+
+	onQuizEnd: function(data){
+		console.log('Quiz end');
+		//
+	},
+
+	onQuestionSoon: function(data){
+		console.log('Question soon');
+		//{"type":"question:soon","time":4,"buttons":[],"passed":true}
+	},
+
+	onQuestionStart: function(data){
+		console.log('Question start');
+		//{"type":"question:start","time":6,"buttons":["A","B"],"passed":true,"countdown":2}
+	},
+
+	onQuestionEnd: function(data){
+		console.log('Question end');
+		//{"type":"question:end","time":8,"buttons":[],"passed":true} 
+	},
+
+	onScoreUpdate: function(data){
+		console.log('Score update');
+		//
+	}	
+};
+
+/*
 // buttons in de gaten houden:
 buttons.watchButtons(function (err, buttonId){
 	if(err) return console.log(err);
@@ -55,3 +102,4 @@ buttons.watchButtons(function (err, buttonId){
 		});
 	}
 });
+*/
